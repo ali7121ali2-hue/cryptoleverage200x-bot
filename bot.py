d7c2dab6d8e6362db6f423e4e588d48a32e27f55
ali7121ali2-hue <ali7121ali2@gmail.com>
2026-06-08 23:21:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -131,7 +131,6 @@ def run_bot():
             print("Polling error:", e)
             import time
             time.sleep(10)
-
 if __name__ == "__main__":
     threading.Thread(target=run_bot, daemon=True).start()
     port = int(os.environ.get("PORT", 10000))
